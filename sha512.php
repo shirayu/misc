d0d9ee745f4385ab63f893af779668c72de8f882
yuta-h <yuta-h@is.naist.jp>
2012-03-29 14:49:09 +0900

    add sha512.php

---
+++ sha512.php
@@ -0,0 +1,47 @@
+<?php
+
+
+if( isset($_POST["lines"])){
+	$list = explode("\r\n", $_POST["lines"] );
+	$sha4html = "";
+
+	foreach($list as $line){
+		$sha = hash(sha512, $line);
+	$original .= <<<__EOT__
+${line}<br />
+__EOT__;
+	$sha4html .= <<<__EOT__
+${sha}<br />
+__EOT__;
+	}
+};
+
+$output4html = <<<__EOT__
+<html> 
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=EUC-JP">
+<title>Get SHA512</title> 
+</head>
+<body>
+		<form action="${_SERVER['SCRIPT_NAME']}" method="post" accept-charset="utf-8"> 
+		<textarea name="lines" rows="4" cols="40"></textarea>
+		<br />
+		<input type="submit" value="GET SHA512" />
+		</form>
+</body>
+</html>
+
+	<h1>Original</h1>	
+	${original}
+
+	<br />
+	<br />
+
+	<h1>SHA512</h1>
+	${sha4html}
+
+__EOT__;
+
+
+print ${output4html};
+
